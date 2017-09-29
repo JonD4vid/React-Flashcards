dec5b6d919880eccaeac23ca5f595fec031069e9
JonD4vid <jondavid_x@hotmail.com>
2017-09-28 22:07:54 -0400

    flashcards

---
+++ src/Config/Firebase/db_config.js
@@ -0,0 +1,8 @@
+export const DB_CONFIG={
+    apiKey: "AIzaSyByyBV9TAXTwafzn-CQkZJmuprNEYs7HOw",
+    authDomain: "react-flashcards-be6d2.firebaseapp.com",
+    databaseURL: "https://react-flashcards-be6d2.firebaseio.com",
+    projectId: "react-flashcards-be6d2",
+    storageBucket: "",
+    messagingSenderId: "997349370246"
+}
